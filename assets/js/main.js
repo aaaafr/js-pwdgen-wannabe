0e7a241ef0b03ffe6bd145319f0bbc2128ff144d
aaaafr <anastasia.frassini@icloud.com>
2021-04-22 19:47:42 +0200

    prove js

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+var firstQuestion = prompt("Qual è il tuo nome ?");
+console.log(firstQuestion);
+
+var myName = "Anastasia";
+console.log(myName);
+
+var secondQuestion = prompt("Qual è il tuo cognome ?");
+console.log(secondQuestion);
+
+var mySurname = "Frassini";
+console.log(mySurname);
+
+var thirdQuestion = prompt ("Qual è il tuo colore preferito?");
+
+var color = "blu"
+console.log(color);
+
+document.getElementById("title").innerHTML = "Ciao mi chiamo " + myName + " " + mySurname + " e il mio colore preferito è il " + color;
+
+
